6a1b955f290585aacdc5727ed41d67834202465d
Rishabh Saxena <rishabh.saxena@32nd.com>
2025-02-06 22:19:40 +0530

    icon-setup and fixes

--- android/app/src/main/java/com/upstox/MainActivity.kt
+++ android/app/src/main/java/com/upstox/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.upstox
+package com.Restaurante
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/upstox/MainApplication.kt
+++ android/app/src/main/java/com/upstox/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.upstox
+package com.Restaurante
 
 import android.app.Application
 import com.facebook.react.PackageList
